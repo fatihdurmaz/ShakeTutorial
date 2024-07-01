32fa805db203ccb388695478d98ed7541803e176
Fatih DURMAZ <fatih.durmaz@outlook.com>
2024-07-01 12:19:47 +0300

    Initial Commit

---
+++ ShakeTutorial/ShakeTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShakeTutorialApp.swift
+//  ShakeTutorial
+//
+//  Created by Fatih Durmaz on 1.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct ShakeTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
